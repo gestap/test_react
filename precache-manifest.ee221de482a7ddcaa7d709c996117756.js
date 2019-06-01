94dc0da40064fd1602331caeb0c4ad74aff7a958
gestap <gestap9@gmail.com>
2019-06-01 22:00:10 +0300

    init

---
+++ precache-manifest.ee221de482a7ddcaa7d709c996117756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "750fa7feb4322bc7bc57275a51c435a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb52e3a1f190fb9a819c",
+    "url": "/static/css/main.e393bfdf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cb52e3a1f190fb9a819c",
+    "url": "/static/js/main.9ed53bc7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
